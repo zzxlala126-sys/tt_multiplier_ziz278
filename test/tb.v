6beb588d0f7e05f034bdd2164b5b1406d989b539
zzxlala126-sys <zzxlala126@gmail.com>
2026-03-05 17:13:37 -0500

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_mult_ziz278 (
+  tt_um_mult_top (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
